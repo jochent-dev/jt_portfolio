18b1430c80dd1356bf43c785ec8186f21094722c
jochentamang-dev <80372592+jochentamang-dev@users.noreply.github.com>
2023-06-27 22:00:24 -0400

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,33 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import './assets/PressStart2P-Regular.ttf'
 // import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// reportWebVitals();
